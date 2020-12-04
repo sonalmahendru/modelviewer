50b957c1b16c9658932fb045d7baccf1501ddec8
sonalmahendru <52343997+sonalmahendru@users.noreply.github.com>
2020-12-04 13:45:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*var assets = ['Breakdance.gltf','ChickenDance.gltf','clapAndSpin.gltf','dougie.gltf','footShuffle.gltf','Gangnam.gltf','hipHop.gltf','Macarena.gltf','Moonwalk.gltf','selfie.gltf']
+
+function selectAnimationToPlay(){
+    let i = Math.floor(Math.random()*10);
+    console.log(assets[i]);
+    let asset = "assets/"+assets[i];
+    document.getElementById("modelviewer").src=asset;
+    assets.splice(i,1);
+    console.log(assets);
+}*/
